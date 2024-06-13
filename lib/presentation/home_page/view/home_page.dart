9412a87a61aba5a3d8642a05d56f83196203a246
nyouibou <akshaits4@gmail.com>
2024-06-13 15:03:15 +0530

    init commit

--- lib/presentation/home_page/view/home_page.dart
+++ lib/presentation/home_page/view/home_page.dart
@@ -1,189 +1,4 @@
-// // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, invalid_use_of_protected_member
-
-// import 'dart:ui';
-
-// import 'package:flutter/material.dart';
-// import 'package:get/get.dart';
-
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:spot_x/core/constants/color.dart';
-// import 'package:spot_x/presentation/home_page/controller/song_data_controller.dart';
-// import 'package:spot_x/presentation/home_page/controller/song_player_controller.dart';
-// import 'package:spot_x/presentation/home_page/view/song_page.dart';
-// import 'package:spot_x/presentation/home_page/view/widgets/custom_listed_page.dart';
-
-// class HomePageScreen extends StatefulWidget {
-//   const HomePageScreen({super.key});
-
-//   @override
-//   State<HomePageScreen> createState() => _HomePageScreenState();
-// }
-
-// class _HomePageScreenState extends State<HomePageScreen> {
-//   SongDataController songDataController = Get.put(SongDataController());
-//   SongPlayerController songPlayerController = Get.put(SongPlayerController());
-//   @override
-//   Widget build(BuildContext context) {
-//     return SafeArea(
-//       child: Scaffold(
-//           backgroundColor: Colors.black,
-//           appBar: AppBar(
-//             title: Text(
-//               "Music",
-//               style: TextStyle(color: Colors.white),
-//             ),
-//             backgroundColor: Colors.transparent,
-//             actions: [
-//               IconButton(
-//                   onPressed: () {},
-//                   icon: Icon(
-//                     Icons.search,
-//                     color: Colors.white,
-//                   )),
-//               IconButton(
-//                   onPressed: () {},
-//                   icon: Icon(
-//                     Icons.more_vert,
-//                     color: Colors.white,
-//                   )),
-//             ],
-//           ),
-//           // appBar: AppBar(
-//           // bottom: TabBar(tabs: [
-//           //   Tab(
-//           //     child: Text("Songs"),
-//           //   ),
-//           //   Tab(
-//           //     child: Text("Artist"),
-//           //   ),
-//           //   Tab(
-//           //     child: Text("Album"),
-//           //   ),
-//           //   Tab(
-//           //     child: Text("Folder"),
-//           //   ),
-//           // ]),
-//           //backgroundColor: Colors.grey,
-//           // title: Text("My Music"),
-//           // actions: [
-//           //   IconButton(
-//           //     onPressed: () {},
-//           //     icon: Icon(Icons.account_circle),
-//           //   ),
-//           // ],
-//           // ),
-//           body: Column(
-//             children: [
-//               Expanded(
-//                 child: Center(
-//                   child: Padding(
-//                     padding:
-//                         const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-//                     child: SingleChildScrollView(
-//                       physics: AlwaysScrollableScrollPhysics(),
-//                       scrollDirection: Axis.vertical,
-//                       child: Column(
-//                           crossAxisAlignment: CrossAxisAlignment.start,
-//                           children: [
-//                             // Container(
-//                             //   height: 50,
-//                             //   // width: MediaQuery.sizeOf(context).width,
-//                             //   decoration: BoxDecoration(
-//                             //       borderRadius: BorderRadius.circular(8),
-//                             //       color:
-//                             //           const Color.fromARGB(255, 145, 143, 143)),
-//                             //   child: Row(
-//                             //     children: [
-//                             //       Container(
-//                             //         height: 50,
-//                             //         width: 200,
-//                             //         // color: Colors.amber,
-//                             //         margin:
-//                             //             EdgeInsets.symmetric(horizontal: 20),
-//                             //         child: TextFormField(
-//                             //           decoration: InputDecoration(
-//                             //               hintText: "Search the Music...",
-//                             //               border: InputBorder.none),
-//                             //         ),
-//                             //       ),
-//                             //       Padding(
-//                             //         padding: const EdgeInsets.symmetric(
-//                             //             horizontal: 15),
-//                             //         child: IconButton(
-//                             //             onPressed: () {},
-//                             //             icon: Icon(Icons.search)),
-//                             //       )
-//                             //     ],
-//                             //   ),
-//                             // ),
-
-//                             Obx(
-//                               () => Row(
-//                                 children: [
-//                                   TextButton(
-//                                     onPressed: () {
-//                                       songDataController.isDeviceSongs.value =
-//                                           true;
-//                                     },
-//                                     child: Text(
-//                                       "Library",
-//                                       style: GoogleFonts.nunitoSans(
-//                                           textStyle: TextStyle(
-//                                               fontSize: 20,
-//                                               fontWeight: FontWeight.w400,
-//                                               color: songDataController
-//                                                       .isDeviceSongs.value
-//                                                   ? ColorConstants.customWhite
-//                                                   : ColorConstants.customGrey3)),
-//                                     ),
-//                                   )
-//                                 ],
-//                               ),
-//                             ),
-
-//                             Padding(
-//                                 padding:
-//                                     const EdgeInsets.symmetric(horizontal: 4),
-//                                 child: Obx(
-//                                     () => songDataController.isDeviceSongs.value
-//                                         ? Column(
-//                                             children: songDataController
-//                                                 .songList.value
-//                                                 .map((e) => CustomListedPage(
-//                                                       songName: e.title,
-//                                                       artist: e.artist!,
-//                                                       onPressed: () {
-//                                                         songPlayerController
-//                                                             .playLocalAudio(e);
-
-//                                                         songDataController
-//                                                             .currentIndex(e.id);
-
-//                                                         Get.to(() => SongPageScreen(
-//                                                             // songName: e.title,
-//                                                             // songArtist: e.artist!,
-//                                                             ));
-//                                                       },
-//                                                     ))
-//                                                 .toList(),
-//                                           )
-//                                         : Column(
-//                                             children: [
-//                                               //CustomListedPage(),
-//                                             ],
-//                                           )))
-//                           ]),
-//                     ),
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           )),
-//     );
-//   }
-// }
-
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, invalid_use_of_protected_member
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -194,7 +9,7 @@ import 'package:spot_x/presentation/home_page/view/song_page.dart';
 import 'package:spot_x/presentation/home_page/view/widgets/custom_listed_page.dart';
 
 class HomePageScreen extends StatefulWidget {
-  const HomePageScreen({super.key});
+  const HomePageScreen({Key? key}) : super(key: key);
 
   @override
   State<HomePageScreen> createState() => _HomePageScreenState();
@@ -239,7 +54,7 @@ class _HomePageScreenState extends State<HomePageScreen> {
                 child: Column(
                   children: [
                     TabBar(
-                      dividerColor: Colors.transparent,
+                      indicatorColor: Colors.transparent,
                       unselectedLabelColor: Colors.grey,
                       tabs: [
                         Tab(
@@ -321,6 +136,40 @@ class _HomePageScreenState extends State<HomePageScreen> {
                     ),
                   ],
                 ),
+              ),
+            ),
+            // Mini player
+            Container(
+              color: Colors.grey[900],
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          songPlayerController.songTitle.value,
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        Text(
+                          songPlayerController.songArtist.value,
+                          style: TextStyle(color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      // Add onPressed function for mini player button
+                    },
+                    icon: Icon(
+                      Icons.play_circle_filled,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
             ),
           ],
